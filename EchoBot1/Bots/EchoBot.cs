776bac3cbd0fa24f7a76352f8a7b012d9e255007
Pratik Patil <pratik@sdcdpu.local>
2021-03-24 10:59:44 +0530

    new

---
+++ EchoBot1/Bots/EchoBot.cs
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.11.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EchoBot1.Bots
+{
+    public class EchoBot : ActivityHandler
+    {
+        public object _accessors { get; private set; }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Handle Message activity type, which is the main activity type for shown within a conversational interface
+            // Message activities may contain text, speech, interactive cards, and binary or unknown attachments.
+            // see https://aka.ms/about-bot-activity-message Jump to learn more about the message and other activity types
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                // Get the conversation state from the turn context.
+                var state = await _accessors.CounterState.GetAsync(turnContext, () => new CounterState());
+
+                // Bump the turn count for this conversation.
+                state.TurnCount++;
+
+                // Set the property using the accessor.
+                await _accessors.CounterState.SetAsync(turnContext, state);
+
+                // Save the new turn count into the conversation state.
+                await _accessors.ConversationState.SaveChangesAsync(turnContext);
+
+                // Echo back to the user whatever they typed.
+                var responseMessage = $"Turn {state.TurnCount}: You sent '{turnContext.Activity.Text}'\n";
+                await turnContext.SendActivityAsync(responseMessage);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync($"{turnContext.Activity.Type} event detected");
+            }
+        }
+    }
+}
